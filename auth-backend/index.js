468fea8ebd492110c1a742bb6b515ef38e4a267d
VairusTsoro <70801958+VairusTsoro@users.noreply.github.com>
2025-08-01 10:33:37 +0400

    change

--- auth-backend/index.js
+++ auth-backend/index.js
@@ -1,15 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const pool = require('./db');
 
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.static('./public'));
 
 const authRoutes = require('./router');
 app.use('/api/auth', authRoutes);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/login.html');
+});
+
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
